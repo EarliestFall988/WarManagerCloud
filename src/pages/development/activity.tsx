f575945bfa8371454299979c0ab864f8293773a5
EarliestFall <howelltaylor195@gmail.com>
2023-06-16 17:24:03 -0500

    configured the messaging window

--- src/pages/development/activity.tsx
+++ src/pages/development/activity.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { api } from "~/utils/api";
 import React from "react";
 import Pusher from "pusher-js";
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 
 interface IMessage {
   message: string;
@@ -39,34 +40,40 @@ const ActivityPage: NextPage = () => {
   };
 
   return (
-    <main className="flex min-h-[100vh] items-center justify-center bg-zinc-800">
-      <div className="w-1/3 rounded border border-zinc-600 bg-zinc-700 p-2 shadow-md">
-        <h1 className="text-lg font-semibold">Activity</h1>
-        <div className="border-t border-zinc-600 py-2" />
-        {messages.map((message, index) => (
-          <div key={index} className="flex flex-col">
-            <p className="py-1 text-sm text-zinc-400">{message.message}</p>
+    <main className="flex min-h-[100vh] items-center justify-center bg-bhall bg-cover bg-center">
+      <div className="flex h-[80vh] w-10/12 flex-col justify-between rounded border border-zinc-600 bg-zinc-700 p-2 shadow-md sm:w-1/3">
+        <h1 className="w-full border-b border-zinc-600 text-lg font-semibold">
+          Activity
+        </h1>
+        <div>
+          {/* <div className="flex h-[50vh] flex-col gap-2 overflow-y-auto truncate bg-red-500"> */}
+          {messages.map((message, index) => (
+            <div key={index} className="flex flex-col">
+              <p className="py-1 text-sm text-zinc-400">{message.message}</p>
+            </div>
+          ))}
+          {/* </div> */}
+
+          <div className="flex gap-1 py-1">
+            <input
+              type="text"
+              className="w-full rounded border border-zinc-500 bg-zinc-600 p-2 outline-none focus:ring-2 focus:ring-amber-600"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  addMessage(text);
+                }
+              }}
+            />
+            <button
+              className="rounded border border-zinc-600 bg-amber-600 p-2 shadow-md"
+              onClick={() => addMessage(text)}
+            >
+              <PaperAirplaneIcon className="h-5 w-5 text-zinc-50" />
+            </button>
           </div>
-        ))}
-        <div className="border-t border-zinc-600 py-2" />
-        <input
-          type="text"
-          className="w-full rounded border border-zinc-600 bg-zinc-600 p-2"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              addMessage(text);
-            }
-          }}
-        />
-        <div className="py-2" />
-        <button
-          className="w-full rounded border border-zinc-600 bg-amber-600 p-2 shadow-md"
-          onClick={() => addMessage(text)}
-        >
-          Send
-        </button>
+        </div>
       </div>
     </main>
   );
