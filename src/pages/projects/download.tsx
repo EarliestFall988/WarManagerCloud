fdd5088001bd4e4b25774cb6b5591c2f58c61de2
Taylor Howell <howelltaylor195@gmail.com>
2023-07-26 23:50:12 -0500

    fixing tag layout issues uploading lots of  data to db

--- src/pages/projects/download.tsx
+++ src/pages/projects/download.tsx
@@ -1,18 +1,25 @@
 import type { Project } from "@prisma/client";
 import type { NextPage } from "next";
-import { useCallback, } from "react";
-import { utils, writeFileXLSX, } from "xlsx";
-import { LoadingPage, LoadingPage2, LoadingSpinner } from "~/components/loading";
+import { useCallback, useState } from "react";
+import { utils, writeFileXLSX, writeXLSX } from "xlsx";
+import {
+  LoadingPage,
+  LoadingPage2,
+  LoadingSpinner,
+} from "~/components/loading";
 import { api } from "~/utils/api";
 
 import dayjs from "dayjs";
 import { NewItemPageHeader } from "~/components/NewItemPageHeader";
-import { ArrowDownCircleIcon, CloudArrowDownIcon } from "@heroicons/react/24/solid";
+import {
+  ArrowDownCircleIcon,
+  CloudArrowDownIcon,
+} from "@heroicons/react/24/solid";
 import { useUser } from "@clerk/nextjs";
 import SignInModal from "~/components/signInPage";
 import { toast } from "react-hot-toast";
-import Image from "next/image";
 import TooltipComponent from "~/components/Tooltip";
+import { reject } from "lib0/promise";
 
 type DealType = {
   id: number;
@@ -118,7 +125,7 @@ type DealType = {
   cc_email: string;
   org_hidden: boolean;
   person_hidden: boolean;
-}
+};
 
 // interface President {
 //   Name: string;
@@ -176,7 +183,6 @@ type DealType = {
 //   );
 // };
 
-
 const DownloadProjectDetails = () => {
   const { data, isLoading, isError } = api.projects.download.useQuery();
 
@@ -199,8 +205,9 @@ const DownloadProjectDetails = () => {
     console.log(json);
 
     const date = new Date();
-    const dateString = `${date.getMonth() + 1
-      }-${date.getDate()}-${date.getFullYear()}`;
+    const dateString = `${
+      date.getMonth() + 1
+    }-${date.getDate()}-${date.getFullYear()}`;
 
     const ws = utils.json_to_sheet(json);
     const wb = utils.book_new();
@@ -215,12 +222,13 @@ const DownloadProjectDetails = () => {
       {data && (
         <>
           <button
-            className=" flex w-full items-center justify-center gap-1 rounded bg-amber-700 hover:bg-amber-600 focus:bg-amber-600 duration-100 transition-all p-2"
+            className=" flex w-full items-center justify-center gap-1 rounded bg-amber-700 p-2 transition-all duration-100 hover:bg-amber-600 focus:bg-amber-600"
             onClick={DownloadAllCrewDetails}
           >
             Download
             <ArrowDownCircleIcon className="h-6 w-6" />
           </button>
+
           <div className="w-full border-t border-zinc-700">
             <div className="grid w-full grid-cols-8 gap-2 border-x border-b border-zinc-600 bg-zinc-600 p-2 text-zinc-100 hover:bg-zinc-500">
               <p className="w-full truncate font-semibold">Name</p>
@@ -271,6 +279,87 @@ const DownloadProjectDetails = () => {
   );
 };
 
+// const DeveloperOptions = () => {
+//   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+//   const { user } = useUser();
+//   let isDeveloper = false;
+
+//   if (user?.emailAddresses[0]?.emailAddress === "taylor.howell@jrcousa.com") {
+//     isDeveloper = true;
+//   }
+
+//   const onFileChange = useCallback((files: FileList | null) => {
+//     if (!files) return;
+//     if (files.length === 0) return;
+
+//     if (files[0] === undefined) return;
+
+//     if (
+//       files[0].type !==
+//       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+//     ) {
+//       toast.error("File must be an xlsx file");
+//       return;
+//     }
+
+//     console.log(files[0]);
+
+//     setSelectedFile(files[0]);
+//   }, []);
+
+//   const onFileUpload = useCallback(() => {
+//     if (!selectedFile) return;
+
+//     const formData = new FormData();
+
+//     formData.append("file", selectedFile);
+
+//     console.log("uploading file");
+
+//     const res = fetch("/api/upload/projects", {
+//       method: "POST",
+//       body: formData,
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+//         toast.success("Uploaded file");
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         toast.error("Error uploading file");
+//       });
+//   }, [selectedFile]);
+
+//   return (
+//     <div className="w-full">
+//       {isDeveloper && (
+//         <div className="w-full rounded border border-zinc-600 bg-zinc-800 p-2">
+//           <p className="text-2xl font-semibold">Developer Details</p>
+//           <div className="flex gap-2">
+//             <input
+//               className="rounded p-2"
+//               type="file"
+//               onChange={(e) => {
+//                 onFileChange(e.currentTarget.files);
+//               }}
+//             />
+//             <button
+//               className=" flex w-full items-center justify-center gap-1 rounded bg-amber-700 p-2 transition-all duration-100 hover:bg-amber-600 focus:bg-amber-600"
+//               onClick={() => {
+//                 onFileUpload();
+//               }}
+//             >
+//               Upload
+//               <ArrowDownCircleIcon className="h-6 w-6 rotate-180" />
+//             </button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
 // type DownloadPipeDriveDetails =
 //   {
@@ -286,122 +375,122 @@ const DownloadProjectDetails = () => {
 //   }
 
 const ReportingPage = () => {
-
-
   const { isSignedIn, isLoaded } = useUser();
 
   // const { data, isLoading, isError } = api.reporting.getPipedriveDeals.useQuery();
 
   const DownloadDealsYTD = useCallback(() => {
-
     const data = fetch("/api/pipedrive/ytdData", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((res) => res.json()).then((data: DealType[]) => {
-      console.log(data);
+    })
+      .then((res) => res.json())
+      .then((data: DealType[]) => {
+        console.log(data);
 
-      if (!data) {
-        toast.error("No data to download");
-        return;
-      }
+        if (!data) {
+          toast.error("No data to download");
+          return;
+        }
 
-      // data?.data.forEach((deal: DealType) => {
-      //     console.log(deal);
-      // })
+        // data?.data.forEach((deal: DealType) => {
+        //     console.log(deal);
+        // })
 
-      const date = new Date();
-      const dateString = `${date.getMonth() + 1
+        const date = new Date();
+        const dateString = `${
+          date.getMonth() + 1
         }-${date.getDate()}-${date.getFullYear()}`;
 
-      // console.log(json);
-      // console.log(dateString);
+        // console.log(json);
+        // console.log(dateString);
 
-      // console.log("data", data);
+        // console.log("data", data);
 
+        const ws = utils.json_to_sheet(data);
+        const wb = utils.book_new();
+        utils.book_append_sheet(wb, ws, "Data");
+        writeFileXLSX(wb, `Deals Year To Date ${dateString}.xlsx`);
 
+        // toast.success("Downloading data");
+      });
 
-      const ws = utils.json_to_sheet(data);
-      const wb = utils.book_new();
-      utils.book_append_sheet(wb, ws, "Data");
-      writeFileXLSX(wb, `Deals Year To Date ${dateString}.xlsx`);
-
-
-      // toast.success("Downloading data");
-    })
-
-    void toast.promise(data, {
-      loading: "Talking to Pipe Drive...",
-      success: "Downloading xlsx file",
-      error: "Error. Try again later.",
-    }, {
-      success: {
-        duration: 7000,
-        icon: "🔥",
+    void toast.promise(
+      data,
+      {
+        loading: "Talking to Pipe Drive...",
+        success: "Downloading xlsx file",
+        error: "Error. Try again later.",
       },
-    });
-
-  }, [])
+      {
+        success: {
+          duration: 7000,
+          icon: "🔥",
+        },
+      }
+    );
+  }, []);
 
   const DownloadWeeklyBids = useCallback(() => {
-
     const data = fetch("/api/pipedrive/weeklybids", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-      }
-    }).then((res) => res.json()).then((data: DealType[]) => {
-      console.log(data);
+      },
+    })
+      .then((res) => res.json())
+      .then((data: DealType[]) => {
+        console.log(data);
 
-      if (!data) {
-        toast.error("No data to download");
-        return;
-      }
+        if (!data) {
+          toast.error("No data to download");
+          return;
+        }
 
-      // data?.data.forEach((deal: DealType) => {
-      //     console.log(deal);
-      // })
+        // data?.data.forEach((deal: DealType) => {
+        //     console.log(deal);
+        // })
 
-      const date = new Date();
-      const dateString = `${date.getMonth() + 1
+        const date = new Date();
+        const dateString = `${
+          date.getMonth() + 1
         }-${date.getDate()}-${date.getFullYear()}`;
 
-      // console.log(json);
-      // console.log(dateString);
+        // console.log(json);
+        // console.log(dateString);
 
-      // console.log("data", data);
+        // console.log("data", data);
 
+        const ws = utils.json_to_sheet(data);
+        const wb = utils.book_new();
+        utils.book_append_sheet(wb, ws, "Data");
+        writeFileXLSX(wb, `Weekly Deals in Bid Phase ${dateString}.xlsx`);
 
+        // toast.success("Downloading data");
+      });
 
-      const ws = utils.json_to_sheet(data);
-      const wb = utils.book_new();
-      utils.book_append_sheet(wb, ws, "Data");
-      writeFileXLSX(wb, `Weekly Deals in Bid Phase ${dateString}.xlsx`);
-
-
-      // toast.success("Downloading data");
-    })
-
-    void toast.promise(data, {
-      loading: "Talking to Pipe Drive...",
-      success: "Downloading xlsx file",
-      error: "Error. Try again later.",
-    }, {
-      success: {
-        duration: 7000,
-        icon: "🔥",
+    void toast.promise(
+      data,
+      {
+        loading: "Talking to Pipe Drive...",
+        success: "Downloading xlsx file",
+        error: "Error. Try again later.",
       },
-    });
-
-  }, [])
-
+      {
+        success: {
+          duration: 7000,
+          icon: "🔥",
+        },
+      }
+    );
+  }, []);
 
   const lastSunday = dayjs().day(-7).format("MM/DD/YYYY");
 
-
   if (!isLoaded) {
-    return <LoadingPage2 />
+    return <LoadingPage2 />;
   }
 
   // if (isLoading) {
@@ -409,7 +498,7 @@ const ReportingPage = () => {
   // }
 
   if (!isSignedIn && isLoaded) {
-    return <SignInModal redirectUrl="/projects/download" />
+    return <SignInModal redirectUrl="/projects/download" />;
   }
 
   // if (isError) {
@@ -418,31 +507,50 @@ const ReportingPage = () => {
 
   return (
     <>
-      <div className="w-full h-full flex flex-col gap-2 justify-start items-start">
-        <div className="flex flex-col items-start justify-center w-full h-full bg-zinc-800 p-2 rounded-sm border border-zinc-700">
+      <div className="flex h-full w-full flex-col items-start justify-start gap-2">
+        <div className="flex h-full w-full flex-col items-start justify-center rounded-sm border border-zinc-700 bg-zinc-800 p-2">
           <h2 className="text-3xl font-semibold">Reporting</h2>
           <p className="text-md tracking-tight">This is the reporting page</p>
         </div>
-        <div className="flex flex-col md:items-start gap-2 justify-center w-full h-full bg-zinc-800 p-2 rounded-sm border border-zinc-700">
-          <h2 className="text-xl font-semibold">Pulling Data From Pipe Drive</h2>
-          <div className="flex gap-2 flex-wrap">
-            <TooltipComponent content={`Download all deals from 1/1/${new Date(Date.now()).getFullYear()} to today (${new Date(Date.now()).toLocaleDateString()}).`} side="bottom">
-              <button onClick={DownloadDealsYTD} className="p-2 flex items-center justify-center rounded bg-amber-700 hover:bg-amber-600 focus:bg-amber-600 duration-100 transition-all">
-                <CloudArrowDownIcon className="h-5 w-5 mr-2" />  <p>{"Download Deals Year To Date (.xlsx)"}</p>
+        <div className="flex h-full w-full flex-col justify-center gap-2 rounded-sm border border-zinc-700 bg-zinc-800 p-2 md:items-start">
+          <h2 className="text-xl font-semibold">
+            Pulling Data From Pipe Drive
+          </h2>
+          <div className="flex flex-wrap gap-2">
+            <TooltipComponent
+              content={`Download all deals from 1/1/${new Date(
+                Date.now()
+              ).getFullYear()} to today (${new Date(
+                Date.now()
+              ).toLocaleDateString()}).`}
+              side="bottom"
+            >
+              <button
+                onClick={DownloadDealsYTD}
+                className="flex items-center justify-center rounded bg-amber-700 p-2 transition-all duration-100 hover:bg-amber-600 focus:bg-amber-600"
+              >
+                <CloudArrowDownIcon className="mr-2 h-5 w-5" />{" "}
+                <p>{"Download Deals Year To Date (.xlsx)"}</p>
               </button>
-
             </TooltipComponent>
-            <TooltipComponent content={`Download all deals in the bid stage since last sunday (${lastSunday}).`} side="bottom">
-              <button onClick={DownloadWeeklyBids} className="p-2 flex items-center justify-center rounded bg-zinc-700 hover:bg-zinc-600 focus:bg-zinc-600 duration-100 transition-all">
-                <CloudArrowDownIcon className="h-5 w-5 mr-2" />  <p>{"Download Weekly Bid Deals (.xlsx)"}</p>
+            <TooltipComponent
+              content={`Download all deals in the bid stage since last sunday (${lastSunday}).`}
+              side="bottom"
+            >
+              <button
+                onClick={DownloadWeeklyBids}
+                className="flex items-center justify-center rounded bg-zinc-700 p-2 transition-all duration-100 hover:bg-zinc-600 focus:bg-zinc-600"
+              >
+                <CloudArrowDownIcon className="mr-2 h-5 w-5" />{" "}
+                <p>{"Download Weekly Bid Deals (.xlsx)"}</p>
               </button>
             </TooltipComponent>
           </div>
         </div>
-      </div >
+      </div>
     </>
-  )
-}
+  );
+};
 
 const DownloadPage: NextPage = () => {
   const { user } = useUser();
