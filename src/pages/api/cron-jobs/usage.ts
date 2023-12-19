fc1d0bf702f061def3dbc4c0f7a68bc406aea545
Taylor Howell <howelltaylor195@gmail.com>
2023-12-19 13:11:09 -0600

    fixing bugs in the session checking cron job

---
+++ src/pages/api/cron-jobs/usage.ts
@@ -0,0 +1,66 @@
+import { clerkClient } from "@clerk/nextjs";
+import { type NextResponse, type NextRequest } from "next/server";
+import { SendMessageToDeveloper } from "~/server/helpers/sendEmailHelper";
+
+export const config = {
+  runtime: "edge",
+};
+
+const devId = process.env.DEV_ID;
+
+if (!devId) {
+  throw new Error("DEV_ID is not set");
+}
+
+const handler = async (req: NextRequest, res: NextResponse) => {
+  const sessions = await clerkClient.sessions.getSessionList();
+
+  const fourteenDaysAgo = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000);
+
+  let newerThan14Days = false;
+
+  if (sessions.length === 0) {
+    await SendMessageToDeveloper(
+      "No sessions found",
+      "No sessions found. Probably should ask Andrew K to see about turning off the War Manager Service.",
+      false
+    );
+    res.ok;
+    return;
+  }
+
+  ///check to see if any session is newer than 14 days
+  sessions.map((session) => {
+    const date = new Date(session.updatedAt);
+
+    if (session.userId != devId) {
+      console.log(
+        date,
+        fourteenDaysAgo,
+        date > fourteenDaysAgo,
+        session.userId
+      );
+    }
+
+    ///if any session is newer than 14 days, return
+    if (date > fourteenDaysAgo && session.userId != devId) {
+      newerThan14Days = true;
+    }
+  });
+
+  if (newerThan14Days) {
+    res.ok;
+
+    return;
+  } else {
+    await SendMessageToDeveloper(
+      "No session is newer than 14 days",
+      "No session is newer than 14 days. Probably should ask Andrew K to see about turning off the War Manager Service.",
+      false
+    );
+  }
+
+  res.ok;
+};
+
+export default handler;
